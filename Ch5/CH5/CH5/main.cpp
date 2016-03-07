af11cd6dd242098176bba75941ecf569c8f5d84a
fredula <fred.thefriedone@gmail.com>
2016-03-07 21:06:16 +0200

    added ch5 as project

---
+++ Ch5/CH5/CH5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CH5
+//
+//  Created by Fred Naude on 2016/03/07.
+//  Copyright (c) 2016 Fred Naude. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
